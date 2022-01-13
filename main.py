4a3cbc4093014539b0e7d3c9856aae45c0eb192e
Ala_ALsalam <alaalsalam101@gmail.com>
2022-01-13 13:07:32 +0300

    challeng 6

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Write sub program add two or three numbers?
+upload the code to GitHub, call will be like that *
+
+"""
+
+
+def sub(num1, num2, num3=0):  # to add two or three numbers
+    return num1 + num2 + num3
+
+
+print(sub(5, 10))  # tow
+print(sub(5, 10, 3))  # three
